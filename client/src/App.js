c975c9b9dbbced7a432fe027f14fcd377f22d598
Jonathan Wong <jonathanfwong@gmail.com>
2022-08-30 19:57:45 +1000

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const checkout = async () => {
+    fetch('http://localhost:5000/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 3 },
+          { id: 2, quantity: 1 },
+        ]
+      })
+    }).then(res => {
+      if(res.ok) return res.json();
+      return res.json().then(json => Promise.reject(json))
+    }).then(({ url }) => {
+      window.location = url;
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+  const getAllItems = async () => {
+    console.log(123)
+    const items = await fetch('http://localhost:5000/storeitems')
+    .then(res => {
+      if(res.ok) return res.json();
+    }).catch((e) => {
+      console.log(e)
+    })
+    console.log('Items:', items)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-h-screen w-full flex justify-center items-center space-x-4">
+      <button
+        onClick={checkout}
+        className="px-6 py-4 text-xl bg-blue-400 hover:bg-blue-500 duration-100 text-white font-semibold rounded-lg"
+      >
+        Checkout
+      </button>
+      <button
+        onClick={getAllItems}
+        className="px-6 py-4 text-xl bg-blue-400 hover:bg-blue-500 duration-100 text-white font-semibold rounded-lg"
+      >
+        Get All Items
+      </button>
     </div>
   );
 }
---
+++ server.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = [
+  { id: 1, priceInCents: 10000, name: 'Learn React Today'},
+  { id: 2, priceInCents: 20000, name: 'Learn CSS Today'}
+]
+
+
+app.get('/storeitems', async (req, res) => {
+  try {
+    res.send(Array.from(storeItems.values()))
+  } catch (error) {
+    res.status(500).json({ error: e.message })
+  }
+})
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.find(storeItem => storeItem.id === item.id)
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: storeItem.name
+            },
+            unit_amount: storeItem.priceInCents
+          },
+          quantity: item.quantity
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}`,
+      cancel_url: `${process.env.CLIENT_URL}`
+    })
+    res.json({ url: session.url })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`))
