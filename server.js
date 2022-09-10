74536994af514c671a190051ae66de53de9c4a16
Jonathan Wong <jonathanfwong@gmail.com>
2022-09-10 17:11:33 +1000

    testing

--- server.js
+++ server.js
@@ -888,9 +888,13 @@ app.post('/checkout', async (req, res) => {
 })
 
 // Handles any requests that don't match the ones above
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname, 'build', 'index.html')); 
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
