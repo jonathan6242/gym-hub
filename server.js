f3e615f71dc3425c16a3d25da8918a594babbae9
Jonathan Wong <jonathanfwong@gmail.com>
2022-09-10 17:23:24 +1000

    require path module

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
+const path = require('path');
 
 app.use(express.json())
 app.use(cors({
